3501b0b80cae893490124a30519025af1bd5e869
farhodbek235 <110379042+farhodbek235@users.noreply.github.com>
2023-07-16 15:41:26 -0700

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,184 @@
+﻿using Azure;
+using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLessons
+{
+    internal class DbFunctions 
+    {
+
+        public void Add()
+        {
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            Console.Write("firstName: ");
+            string firstName = Console.ReadLine();
+            Console.Write("lastName: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            string sql = $"insert into users(firstName,lastname,age)values('{firstName}','{lastName}','{age}')";
+
+            var users = con.Query<user>(sql);
+        }
+
+        public void DeleteAll()
+        {
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            string sql = $"delete from users where id = id";
+
+            var users = con.Query<user>(sql);
+        }
+
+        public void DeleteById()
+        {
+            GetAll();
+            Console.WriteLine();
+            Console.Write("Id: ");
+            int name = int.Parse(Console.ReadLine());
+
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            string sql = $"delete from users where id = '{name}'";
+
+            var users = con.Query<user>(sql);
+        }
+
+        public void DeleteByName()
+        {
+            GetAll();
+            Console.WriteLine();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            string sql = $"delete from users where firstname = '{name}'";
+
+            var users = con.Query<user>(sql);
+        }
+
+        public void GetAll()
+        {
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            string sql = $"select * from users";
+
+            var users = con.Query<user>(sql);
+
+            foreach (var i in users)
+            {
+                Console.WriteLine($"Id: {i.id}\nFirstName: {i.firstName}\nLastName: {i.lastName}\nAge: {i.age}");
+                Console.WriteLine();
+            }
+        }
+
+        public void GetById()
+        {
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            string sql = $"select * from users where id = '{id}'";
+
+            var users = con.Query<user>(sql);
+
+            foreach (var i in users)
+            {
+                Console.WriteLine($"Id: {i.id}\nFirstName: {i.firstName}\nLastName: {i.lastName}\nAge: {i.age}");
+                Console.WriteLine();
+            }
+        }
+
+        public void GetByName()
+        {
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            Console.Write("Name: ");
+
+            string name = Console.ReadLine();
+
+            string sql = $"select * from users where firstName = '{name}'";
+
+            var users = con.Query<user>(sql);
+
+            foreach (var i in users)
+            {
+                Console.WriteLine($"Id: {i.id}\nFirstName: {i.firstName}\nLastName: {i.lastName}\nAge: {i.age}");
+                Console.WriteLine();
+            }
+        }
+
+        public void Update()
+        {
+            GetAll();
+
+            Console.Write("UserId: ");
+            int id = int.Parse(Console.ReadLine());
+
+            string connectionString = "Server=localhost;Port=5433;User Id=postgres;Password=ing0077K;Database=mydata;";
+
+            var con = new NpgsqlConnection(connectionString);
+
+            con.Open();
+
+            Console.Write("FirstName: ");
+            string firstName = Console.ReadLine();
+            Console.Write("LastName: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            string sql = $"update users set firstName = '{firstName}',lastName = '{lastName}',age = '{age}' where id = '{id}'";
+
+            var users = con.Query<user>(sql);
+        }
+    }
+}
+internal interface IDbFunctions
+{
+    public void Add();
+    public void Update();
+    public void DeleteAll();
+    public void DeleteById();
+    public void DeleteByName();
+    public void GetAll();
+    public void GetById();
+    public void GetByName();
+}
